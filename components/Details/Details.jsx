d7b59d4547f9705ebd7cb628bdeba18eb361e3db
Khalil Saad Harouri <saadharouri@gmail.com>
2022-01-13 16:21:47 +0100

    App init

---
+++ components/Details/Details.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@mui/material';
+import { Doughnut } from 'react-chartjs-2';
+
+import useStyles from './styles';
+//import useTransactions from '../../useTransactions';
+
+const Details = ({ title, subheader }) => {
+  //const { total, chartData } = useTransactions(title);
+  const classes = useStyles();
+
+  return (
+    <Card className={title === 'Income' ? classes.income : classes.expense}>
+      <CardHeader title={title} subheader={subheader} />
+      <CardContent>
+        <Typography variant="h5">$</Typography>
+        {/* <Doughnut data={chartData} /> */}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Details;
