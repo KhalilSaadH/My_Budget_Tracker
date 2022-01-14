4ffd995bb78564482ba03ac52f889173f3231184
Khalil Saad Harouri <saadharouri@gmail.com>
2022-01-14 18:04:04 +0100

    Adding form component

--- components/Details/Details.jsx
+++ components/Details/Details.jsx
@@ -3,18 +3,18 @@ import { Card, CardHeader, CardContent, Typography } from '@mui/material';
 import { Doughnut } from 'react-chartjs-2';
 
 import useStyles from './styles';
-//import useTransactions from '../../useTransactions';
+import useTransactions from '../../useTransactions';
 
 const Details = ({ title, subheader }) => {
-  //const { total, chartData } = useTransactions(title);
+  const { total, chartData } = useTransactions(title);
   const classes = useStyles();
 
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
       <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <Typography variant="h5">$</Typography>
-        {/* <Doughnut data={chartData} /> */}
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
