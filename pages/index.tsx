8e0b379311708d8f2f9096fb9e007f8fcac9f239
Khalil Saad Harouri <saadharouri@gmail.com>
2022-01-18 15:14:54 +0100

    Updating homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import React, { Component, FunctionComponent, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Grid } from '@mui/material';
 
 import { SpeechState, useSpeechContext } from "@speechly/react-client";
@@ -12,16 +12,17 @@ import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui'
 //import   Details   from '../components/Details/Details.jsx'
 import styles from '../styles/Home.module.css'
 import dynamic from 'next/dynamic';
+const Main = dynamic(
+  () => import('../components/Main/Main.jsx'),
+  { ssr: false }
+);
+const Details = dynamic(
+  () => import('../components/Details/Details.jsx'),
+  { ssr: false }
+);
 
 const Home: NextPage = () => {
-  const Main = dynamic<any>(
-    () => import('../components/Main/Main.jsx'),
-    { ssr: false }
-  );
-  const Details = dynamic<{title:any; subheader:any;}>(
-    () => import('../components/Details/Details.jsx'),
-    { ssr: false }
-  );
+  
   const { speechState } = useSpeechContext();
   const mainRef = useRef<HTMLInputElement>(null);
 
